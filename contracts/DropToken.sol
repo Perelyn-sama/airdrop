2526a392a8c4ff23bc27df06b2dc07158634be84
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-01-09 19:11:32 +0100

    tweaking

--- contracts/DropToken.sol
+++ contracts/DropToken.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity >=0.4.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract DropToken is ERC20, Ownable {
-    constructor() ERC20("Drop Token", "DROP") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
-    }
+contract DropToken is ERC20, Ownable,ReentrancyGuard {
 
     // Array of address for the airdrop
     address[] public whiteList;
 
-    // Amount each participant will receive 
-    uint airdropAmount = 5000 * 10 ** decimals();
+    // Amount each participant will receive
+    uint airdropAmount = 500 * 10 ** decimals();
+
+    constructor() ERC20("Drop Token", "DROP") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
 
     // Function to add participants for the airdrop
     function addAirdropParticipant(address participant) public returns(bool success){
@@ -22,7 +24,7 @@ contract DropToken is ERC20, Ownable {
     }
 
     // Function to trigger the airdrop
-    function airdrop() public onlyOwner() returns(bool success) {
+    function startAirdrop() public onlyOwner() nonReentrant() returns(bool success) {
         for(uint i = 0; i < whiteList.length; i++){
             transfer(whiteList[i], airdropAmount);
         }
--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,15 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
     }
-  }
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version
+    }
+  },
 };
